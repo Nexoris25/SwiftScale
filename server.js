15ffcd423ab159cae5a02090f475e662114db16a
Nexoris25 <nexoristechnologiesltd@gmail.com>
2025-12-02 10:17:28 +0100

    updated server.js

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
-// server.js
-const express = require("express");
-const next = require("next");
+const { createServer } = require('http');
+const next = require('next');
 
-const dev = false; // cPanel = always production
-const app = next({ dev });
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
-const port = process.env.PORT || 3000;
-
 app.prepare().then(() => {
-  const server = express();
-
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, "127.0.0.1", () => {
-    console.log(`Next.js app running on port ${port}`);
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(process.env.PORT || 3000, () => {
+    console.log('Next.js server running...');
   });
 });
